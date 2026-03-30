de305c4cc7004fd757f1a9373ad6076dacbeaf0d
Baldezhnik232 <stanislav.bondarev.2015@gmail.com>
2026-03-30 18:24:35 +0300

    test

---
+++ app/ui/screen.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+export const Screen = () => {
+  return (
+    <div>
+      <h1 className=" 
+      bg-[linear-gradient(to_right,#2100EC_0%,#FFFFFF_58%,#DC71F7_90%)]
+      text-[3rem]
+      shimmer
+      bg-[length:300%_100%]
+      ">
+        GLANCE
+      </h1>
+    </div>
+    
+  );
+};
